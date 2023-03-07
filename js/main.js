528e24490ff2496f5b55784208a488eec3c87177
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-07 17:45:11 +0100

    aggiorno prompt

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ let contentDom = document.getElementById('content');
 let numberGenerated;
 let randomNumber =  [];
 
-contentDom.innerHTML = randomNumber;
 
-
+// ciclo 5 volte per inserire random number in array
 for (let i = 0; i<5; i++){
         numberGenerator(1, 100)
     if(!randomNumber.includes(numberGenerated))
@@ -17,9 +16,33 @@ for (let i = 0; i<5; i++){
         randomNumber.push(numberGenerated)
     }
 }
+//creo numero
 function numberGenerator (min, max){
  numberGenerated = Math.floor(Math.random() * (max - min +1) + min);
  return numberGenerated;
 }
-console.log(numberGenerated);
+//mostro i numeri
+   contentDom.innerHTML=randomNumber;
+// setto il timer a 30 secondi  e nascondo i numeri
+
+
+
+   let numberUser
+   setTimeout(fiveUserNumber(), 4000);
+
+setTimeout(function(){
+    contentDom.innerHTML = "";
+  }, 3000);
+// chiedo con prompt i numeri all'utente e li salvo in array
+let userNumberList = [];
+
+
 console.log(randomNumber);
+console.log(userNumberList);
+
+
+function fiveUserNumber(){
+for ( let i = 0; i < randomNumber.lenght -1; i++){
+    numberUser = prompt('inserisci i numeri uno alla volta');
+    userNumberList.push(numberUser)
+    }};
